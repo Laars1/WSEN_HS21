b1a2927204790e8e1455edc839a9aeb929b286b5
Roman Krass <roman.krass@hotmail.ch>
2021-09-30 13:23:29 +0200

    update

--- Roman/Print_True_False.py
+++ Roman/Print_True_False.py
@@ -1,4 +1,4 @@
 for P in (True,False):
     for Q in (True,False):
         for R in (True,False):
-            print("P = ", P, "Q = ", Q, "R = ",R, ", Lösung der Rechnung = ", (P and (not Q or R)) and (not (not R and P)))
+            print("P = ", P, "Q = ", Q, "R = ",R, " Rechnung: (P and (not Q or R)) and (not (not R and P))", " Lösung der Rechnung = ", (P and (not Q or R)) and (not (not R and P)))
