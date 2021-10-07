d339f2250fc1d170b6bf32de23af26cedb2e86be
Roman Krass <roman.krass@hotmail.ch>
2021-10-07 19:19:54 +0200

    Tannenbaum

---
+++ Roman/KW40/Tannenbaum.py
@@ -0,0 +1,10 @@
+
+zahl = int(input("Gebe eine Zahl ein: "))
+
+#plus 1, da range immer eins weniger als das Maximum macht
+zahlen = range(1,zahl +1,2)
+
+i = 0
+for a in zahlen:
+    print(" " * (zahl -i), "*" * a)
+    i += 1
