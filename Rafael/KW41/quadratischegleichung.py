fe5dd0a42f4bc59b1c0d224d080a27f33a291ee0
mahlr1 <rafael-mahl@hotmail.com>
2021-10-21 14:45:51 +0200

    .

--- Rafael/KW41/quadratischegleichung.py
+++ Rafael/KW41/quadratischegleichung.py
@@ -1,10 +1,12 @@
+
 def qg(A, B, C):
-    d = B * B - 4 * A * C
-    if d > 0:
-        return (42, 42)
+    from math import sqrt
+    D = B * B - 4 * A * C
+    if D > 0:
+        return ((-B + sqrt(D)) / (2 * A), (-B - sqrt (D)) / (2 * A))
     elif d == 0:
-        return - B / (2 * A)
+        return (- B / (2 * A))
     else:
         return ()
 
-print (qg(1, -4, 4))
+print(qg(1, -3, 2))
